731ca6eff293f466300f1a615ef45da9d526daa9
Satya Anurag Das <57854503+satya-root@users.noreply.github.com>
2022-10-06 12:34:13 +0530

    guess the num

---
+++ Guess The Num/play_game.py
@@ -0,0 +1,13 @@
+from gen_num import Number
+a=input('Enter you name: ')
+number= Number(a)
+number.gen_num()
+quit=input('Type quit to exit  ').lower
+while 'quit' in quit:
+    exit()
+
+    
+
+
+
+    
